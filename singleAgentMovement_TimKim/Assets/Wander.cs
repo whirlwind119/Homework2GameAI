24adfbebd9f93a5fb5158a5a5ec8a60061057f63
whirlwind119 <tim.kim1996@gmail.com>
2017-09-18 22:58:17 -0400

    add and fix more stuff

--- singleAgentMovement_TimKim/Assets/Wander.cs
+++ singleAgentMovement_TimKim/Assets/Wander.cs
@@ -3,26 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Wander : MonoBehaviour {
-	private Vector3 direction = new Vector3();
 
+    Rigidbody2D rb;
+    float timer = 7;
 
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 target = direction  - transform.right * 5;
-		target = new Vector3 (target.x + Random.Range (-1.0f, 1.0f), target.y + Random.Range (-1.0f, 1.0f), 0.0f) * 2;
-		target.Normalize ();
-		direction = target + transform.right * 5;
 
-		Vector3 vectorToTarget = direction - transform.position;
-		float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
-		Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
-		transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * 1.5f);
-
+        if (timer >= 7f) {
+            rb.angularVelocity = Random.Range(-100f, 100f);
+            timer = 0;
+        }
+        timer += Time.deltaTime;
 		transform.Translate(Vector3.right * Time.deltaTime * 3);
 
 	}
