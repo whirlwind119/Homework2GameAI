10e8cba48e535141f1d3db31208e1ac9d30bfc50
whirlwind119 <tim.kim1996@gmail.com>
2017-09-18 20:38:24 -0400

    Add Flee

---
+++ singleAgentMovement_TimKim/Assets/Flee.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flee : MonoBehaviour {
+
+    public GameObject fleeTarget;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 vectorToTarget = transform.position - fleeTarget.transform.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * .7f);
+    }
+}
--- singleAgentMovement_TimKim/Assets/Pursue.cs
+++ singleAgentMovement_TimKim/Assets/Pursue.cs
@@ -7,9 +7,10 @@ public class Pursue : MonoBehaviour {
     public float speed;
     public GameObject target;
 
+
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
@@ -32,10 +33,14 @@ public class Pursue : MonoBehaviour {
         Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * .7f);
 
+
+        /*
         //move forward
         //transform.position = new Vector3(transform.position.x + (this.speed * Time.deltaTime * Mathf.Cos(Quaternion.EulerAngles(transform.))), transform.position.y); 
         Vector3 rot = transform.rotation * Vector3.forward;
         Debug.Log(transform.position.x * rot.x * speed * Time.deltaTime); 
         transform.position = new Vector3(transform.position.x + rot.x * speed * Time.deltaTime, transform.position.y + rot.y * speed * Time.deltaTime, transform.position.z);
+        */
+
     }
 }
